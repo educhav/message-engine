6f346cac9b1ac6f4a3f6a6365f748358473149ad
educhav <educhav@protonmail.com>
2021-04-07 05:47:40 -0500

    engine.h added, json parsing fixed

---
+++ src/engine.h
@@ -0,0 +1,14 @@
+#include<iostream>
+
+namespace Facebook {
+    const std::string participants[] = {"Eddie Chavez", "Degan Nestrichal", "JJ Joseph", 
+                                        "Samuel Adams", "Austin Medina",
+                                        "J Cole Patt", "Elijah Parker", "Maci Marckx", 
+                                        "Christopher Catunao", "Chloe Cumberworth", 
+                                        "Berto Medina", "Valerie Kaley", "Dolly Saphire", 
+                                        "Rebecca St Ange", "Caleb Rondeau","Rande Plewka", 
+                                        "Lexi Fergelic"};
+    const std::string filtered[] = {"joined the group chat."};
+
+    const int FILE_COUNT = 21;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,61 @@
-#include<iostream>
-#include<fstream>
-#include<vector>
-#include<nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+
+#include "engine.h"
 
 using json = nlohmann::json;
 
-typedef struct 
+class Message
 {
-    const std::string name;
-    std::vector<std::string> messages;
-    std::vector<int> timestamps;
-} Person;
+    public:
+        std::string message;
+        long timestamp;
+};
 
-typedef struct
+class Sender
 {
-    
-} Message;
+    public:
+        std::string name;
+        std::vector<Message> messages;
+};
 
-void read_file(const int file_number) 
+void read_file(const int file_number, Sender* sender)
 {
-    std::ifstream stream("/home/eddie/code/c++/message-engine/messages/message_" + std::to_string(file_number) + ".json");
+
+    std::ifstream stream("/home/eddie/code/c++/message-engine/messages/message_" 
+            + std::to_string(file_number) + ".json");
     json file_data = json::parse(stream);
-    std::cout << file_data << std::endl;
+    auto messages = file_data["messages"];
+
+    unsigned int participant_count = sizeof(Facebook::participants) / sizeof(Facebook::participants[0]);
+    sender->messages = std::vector<Message>(messages.size() / participant_count);
+
+    for (unsigned long i = 0; i < messages.size(); i++) 
+    {
+        if (messages.at(i)["content"] == nullptr)
+        {
+            continue;
+        }
+        std::string message = messages.at(i)["content"];
+        std::string author = messages.at(i)["sender_name"];
+        long timestamp = messages.at(i)["timestamp_ms"];
+
+        if(author.compare(sender->name) == 0) 
+        {
+            Message msg = {message, timestamp};
+            sender->messages.push_back(msg);
+        }
+    }
 }
 
-void read_all(Person person) 
+void read_all(Sender* sender) 
 {
+    for(unsigned int i = 0; i < Facebook::FILE_COUNT; i++) 
+    {
+    }
 }
 
 void display_page(std::string page) 
@@ -38,5 +68,4 @@ void display_page(std::string page, std::string delim)
 
 int main(void) 
 {
-    read_file(3);
 }
